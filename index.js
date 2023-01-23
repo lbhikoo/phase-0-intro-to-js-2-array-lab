42bddde13c3d074b02a290dd7cb87cb58c82e057
Lisamarie Bhikoo <lisamariebhikoo@icloud.com>
2023-01-22 20:01:30 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+
+   let cats = ["Milo", "Otis", "Garfield"]  
+
+   function destructivelyAppendCat(name) {
+    cats.push(name) 
+
+   }
+
+   function destructivelyPrependCat(name) {
+    cats.unshift(name)
+   }
+   function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+   }
+   function destructivelyRemoveFirstCat(name){
+    cats.shift()
+   }
+   function appendCat(name) {
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+   }
+   function prependCat(name){
+    var newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+   }
+   function removeLastCat(name){
+    var newArray = cats.slice();
+    newArray.pop(name)
+    return newArray
+   }
+   function removeFirstCat(name){
+    var newArray = cats.slice();
+    newArray.shift(name)
+    return newArray
+   }
